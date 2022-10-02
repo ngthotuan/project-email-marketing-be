f273f394e2938f4427ced958bdc44b898503f513
tuan2012 <phampham1109@gmail.com>
2022-10-02 10:59:43 +0700

    init project

---
+++ src/main/java/com/example/projectemailmarketingbe/ProjectEmailMarketingBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectemailmarketingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEmailMarketingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEmailMarketingBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectemailmarketingbe/ProjectEmailMarketingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectemailmarketingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEmailMarketingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
