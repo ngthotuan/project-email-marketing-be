9611b9fd4371889e9dbf6a70c37016090fd925ea
ngthotuan <ngthotuan@gmail.com>
2022-10-15 22:01:00 +0700

    fix bug edit proxy

--- src/main/java/com/example/projectemailmarketingbe/dto/ProxyUpdateDto.java
+++ src/main/java/com/example/projectemailmarketingbe/dto/ProxyUpdateDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProxyUpdateDto {
-    private Long proxyId;
+    private Long id;
     private String name;
     private String username;
     private String password;
