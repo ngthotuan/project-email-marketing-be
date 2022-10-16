56d0f49a372e1a84bc2a5469b906a40b5e7f8c95
ngthotuan <ngthotuan@gmail.com>
2022-10-16 10:15:47 +0700

    fix bug template content

--- src/main/java/com/example/projectemailmarketingbe/model/TemplateEntity.java
+++ src/main/java/com/example/projectemailmarketingbe/model/TemplateEntity.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,5 +18,6 @@ public class TemplateEntity {
     private Long id;
     private String name;
     private String subject;
+    @Column(columnDefinition = "LONGTEXT")
     private String content;
 }
