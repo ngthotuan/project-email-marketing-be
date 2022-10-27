97712311ddc48ab0422756e3490e79a7ce244b41
Tuan Chau Xuan <tuan.chauxuan@evri.com>
2022-10-27 20:27:23 +0700

    update time zone vietnam

--- src/main/java/com/example/projectemailmarketingbe/ProjectEmailMarketingBeApplication.java
+++ src/main/java/com/example/projectemailmarketingbe/ProjectEmailMarketingBeApplication.java
@@ -1,15 +1,26 @@
 package com.example.projectemailmarketingbe;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.time.LocalDate;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+
 @EnableScheduling
 @EnableSwagger2
 @SpringBootApplication
+@Slf4j
 public class ProjectEmailMarketingBeApplication {
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        log.info("Spring boot application running in Europe/London timezone :" + LocalDate.now());
+    }
     public static void main(String[] args) {
         SpringApplication.run(ProjectEmailMarketingBeApplication.class, args);
     }
